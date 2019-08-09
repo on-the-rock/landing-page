12df8266df2f990e17719f96a7bb58b850c2bbb9
seki <schokolade0317@gmail.com>
2019-08-09 23:47:48 +0900

    fixed ui for mobile

--- src/frontend/home/page.js
+++ src/frontend/home/page.js
@@ -8,13 +8,16 @@ class Home extends Component {
         const {handleOnChangeLanguage} = this.props;
         return (
             <div className="pusher">
-                <div className="ui vertical masthead center aligned segment">
-                    <div className="ui container">
+                <div className='col'>
+                    <img className='ui fluid image ' src='https://d1wwbe44qvngmh.cloudfront.net/images/banner.png'/>
+                    <a href=""><img className='start-button'
+                                    src='https://d1wwbe44qvngmh.cloudfront.net/images/start-game.png'/></a>
+                    <div className="ui container absolute-menu">
                         <div className="ui large secondary inverted menu">
-                            <a className="active item">Home</a>
-                            <a className="item">Market</a>
-                            <a className="item">Company</a>
                             <div className="right item">
+                                <a className="active item">Home</a>
+                                <a className="item">Market</a>
+                                <a className="item">Company</a>
                                 <Dropdown text='Language'>
                                     <Dropdown.Menu>
                                         <Dropdown.Item text='English' onClick={e => {
@@ -28,121 +31,109 @@ class Home extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className='col'>
-                        <img className='ui fluid image ' src='https://d1wwbe44qvngmh.cloudfront.net/images/banner.png'/>
-                        <a href=""><img className='start-button'
-                                src='https://d1wwbe44qvngmh.cloudfront.net/images/start-game.png'/></a>
-                    </div>
                 </div>
-
-                <div className="ui vertical stripe segment">
-                    <div className="ui middle aligned stackable grid container">
-                        <div className="row">
-                            <div className="eight wide column">
-                                <h3 className="ui white header"><FormattedMessage id="top.yourOnlyCardTitle"/></h3>
-                                <p className="gray"><FormattedMessage id="top.yourOnlyCardSentence"/></p>
-                                <h3 className="ui white header"><FormattedMessage id="top.sellYourCardTitle"/></h3>
-                                <p className="gray"><FormattedMessage id="top.sellYourCardSentence"/></p>
-                            </div>
-                            <div className="eight wide right floated column">
-                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/flow.jpg'
-                                     className="ui fluid image"
-                                     alt="flow"/>
-                            </div>
+                <div className="ui middle aligned stackable grid container">
+                    <div className="row">
+                        <div className="seven wide column">
+                            <h2 className="ui white header"><FormattedMessage id="top.howDoesItWork"/></h2>
+                            <h3 className="ui white header"><FormattedMessage id="top.yourOnlyCardTitle"/></h3>
+                            <p className="gray"><FormattedMessage id="top.yourOnlyCardSentence"/></p>
+                            <h3 className="ui white header"><FormattedMessage id="top.sellYourCardTitle"/></h3>
+                            <p className="gray"><FormattedMessage id="top.sellYourCardSentence"/></p>
+                        </div>
+                        <div className="nine wide right floated column">
+                            <img src='https://d1wwbe44qvngmh.cloudfront.net/images/flow.png'
+                                 className="ui fluid image"
+                                 alt="flow"/>
                         </div>
                     </div>
                 </div>
 
-                <div className="ui vertical stripe segment">
-                    <div className="ui stackable grid container">
-                        <div className="row">
-                            <div className="six wide column">
-                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/blockchain.png'
-                                     className="ui fluid image"
-                                     alt="blockchain"/>
-                            </div>
-                            <div className="ten wide middle aligned column">
-                                <h3 className="ui white header"><FormattedMessage id="top.whatIsDappsTitle"/></h3>
-                                <p className="gray"><FormattedMessage id="top.whatIsDappsSentence"/></p>
-                            </div>
+                <div className="ui stackable grid">
+                    <div className="row">
+                        <div className="six wide column">
+                            <img src='https://d1wwbe44qvngmh.cloudfront.net/images/blockchain.png'
+                                 className="ui fluid image"
+                                 alt="blockchain"/>
+                        </div>
+                        <div className="nine wide middle aligned column">
+                            <h2 className="ui white header"><FormattedMessage id="top.whatIsDappsTitle"/></h2>
+                            <p className="gray"><FormattedMessage id="top.whatIsDappsSentence"/></p>
                         </div>
                     </div>
                 </div>
 
-                <div className="ui vertical stripe segment">
-                    <div className="ui stackable grid container">
-                        <div className="row">
-                            <div className="ten wide middle aligned column">
-                                <h3 className="ui white header"><FormattedMessage id="top.strengthTitle"/></h3>
-                                <p className="gray"><FormattedMessage id="top.strengthSentence"/></p>
-                            </div>
-                            <div className="six wide column">
-                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/strength.png'
-                                     className="ui fluid image"
-                                     alt="strength"/>
-                            </div>
+                <div className="ui stackable grid container">
+                    <div className="row">
+                        <div className="eight wide middle aligned column">
+                            <h2 className="ui white header"><FormattedMessage id="top.strengthTitle"/></h2>
+                            <p className="gray"><FormattedMessage id="top.strengthSentence"/></p>
+                        </div>
+                        <div className="eight wide column">
+                            <img src='https://d1wwbe44qvngmh.cloudfront.net/images/strength.png'
+                                 className="ui fluid image"
+                                 alt="strength"/>
                         </div>
                     </div>
                 </div>
 
-                <div className="ui vertical stripe segment">
-                    <div className="ui stackable grid container">
-                        <div className="row">
-                            <div className="six wide column">
-                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/story.png'
-                                     className="ui fluid image"
-                                     alt="story"/>
-                            </div>
-                            <div className="ten wide middle aligned column">
-                                <h3 className="ui white header"><FormattedMessage id="top.storyTitle"/></h3>
-                                <p className="gray"><FormattedMessage id="top.storySentence"/></p>
-                            </div>
+                <div className="ui stackable grid container">
+                    <div className="row">
+                        <div className="eight wide column">
+                            <img src='https://d1wwbe44qvngmh.cloudfront.net/images/story.png'
+                                 className="ui fluid image"
+                                 alt="story"/>
+                        </div>
+                        <div className="eight wide middle aligned column">
+                            <h2 className="ui white header"><FormattedMessage id="top.storyTitle"/></h2>
+                            <p className="gray"><FormattedMessage id="top.storySentence"/></p>
                         </div>
                     </div>
                 </div>
 
-                <div className="ui vertical stripe segment">
-                    <div className="ui text">
-                        <h3 className="ui center aligned white header"><FormattedMessage id="top.systemTitle"/></h3>
-                        <div className="ui center aligned grid">
-                            <div className="five wide column">
-                                <h4 className="ui white header"><FormattedMessage id="top.makeCardTitle"/></h4>
-                                <p className="gray"><FormattedMessage id="top.makeCardSentence"/></p>
-                                <div className="col">
-                                    <img src="https://d1wwbe44qvngmh.cloudfront.net/images/create-card.gif"
-                                         className="create-card-gif"/>
-                                    <img src="https://d1wwbe44qvngmh.cloudfront.net/images/card-bg.png"
-                                         className="card-bg"/>
-                                </div>
+                <div className="ui text">
+                    <h2 className="ui center aligned white header"><FormattedMessage id="top.systemTitle"/></h2>
+                    <div className="ui center aligned grid">
+                        <div className="five wide column">
+                            <h3 className="ui white header"><FormattedMessage id="top.makeCardTitle"/></h3>
+                            <p className="gray"><FormattedMessage id="top.makeCardSentence"/></p>
+                            <div className="col">
+                                <img src="https://d1wwbe44qvngmh.cloudfront.net/images/create-card.gif"
+                                     className="create-card-gif"/>
+                                <img src="https://d1wwbe44qvngmh.cloudfront.net/images/card-bg.png"
+                                     className="card-bg"/>
                             </div>
-                            <div className="five wide column">
-                                <h4 className="ui white header"><FormattedMessage id="top.makeDeckTitle"/></h4>
-                                <p className="gray"><FormattedMessage id="top.makeDeckSentence"/></p>
-                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/deck.png'
-                                     className="ui massive image"
+                        </div>
+                        <div className="five wide column">
+                            <h3 className="ui white header"><FormattedMessage id="top.makeDeckTitle"/></h3>
+                            <p className="gray"><FormattedMessage id="top.makeDeckSentence"/></p>
+                            <img src='https://d1wwbe44qvngmh.cloudfront.net/images/deck.png'
+                                 className="ui massive image"
+                                 alt="battle"/>
+                        </div>
+                        <div className="five wide column">
+                            <h3 className="ui white header"><FormattedMessage id="top.battleTitle"/></h3>
+                            <p className="gray"><FormattedMessage id="top.battleSentence"/></p>
+                            <div className="col">
+                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/battle-fg.png'
+                                     className=" ui fluid image center aligned bigger"
+                                     alt="battle"/>
+                                <img src='https://d1wwbe44qvngmh.cloudfront.net/images/battle-bg.png'
+                                     className="battle-bg"
                                      alt="battle"/>
                             </div>
-                            <div className="five wide column">
-                                <h4 className="ui white header"><FormattedMessage id="top.battleTitle"/></h4>
-                                <p className="gray"><FormattedMessage id="top.battleSentence"/></p>
-                                <div className="col">
-                                    <img src='https://d1wwbe44qvngmh.cloudfront.net/images/battle-fg.png'
-                                         className=" ui fluid image center aligned"
-                                         alt="battle"/>
-                                    <img src='https://d1wwbe44qvngmh.cloudfront.net/images/battle-bg.png'
-                                         className="battle-bg"
-                                         alt="battle"/>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="ui inverted vertical footer segment">
-                    <div className="ui container center aligned">
-                        <p className="gray"><FormattedMessage
-                            id="top.companyName"/></p>
-                    </div>
+                <div className="ui vertical footer segment col">
+                    <img src='https://d1wwbe44qvngmh.cloudfront.net/images/footer.png'
+                         alt="footer"
+                         className="ui image"/>
+                    <p className="gray footer-right"><FormattedMessage
+                        id="top.companyName"/></p>
+                    <img src="https://d1wwbe44qvngmh.cloudfront.net/images/Frame+C+Battle.png"
+                         alt="frame c battle" className="footer-left"/>
                 </div>
             </div>
         );
