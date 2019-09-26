e007f47cf11a3999798148fe38a8005c5cea1f66
seki <schokolade0317@gmail.com>
2019-09-27 00:34:46 +0900

    add market card

--- src/common/sellCard.js
+++ src/common/sellCard.js
@@ -1,7 +1,135 @@
 const sellCards = {
     preSale: [
-        {cardNo: 1100, cardName:'dragon', imageURL:'https://d1wwbe44qvngmh.cloudfront.net/images/dragon.png',priceEther:1},
-        {cardNo: 1101, cardName: 'unicorn', imageURL:'https://d1wwbe44qvngmh.cloudfront.net/images/unicorn.png',priceEther:1}
+        {cardNo: 1100, cardNameJa:'ドラゴン', cardName:'dragon', imageURL:'https://d1wwbe44qvngmh.cloudfront.net/images/dragon.png',priceEther:1},
+        {cardNo: 1101, cardNameJa: 'ユニコーン',cardName:'unicorn', imageURL:'https://d1wwbe44qvngmh.cloudfront.net/images/unicorn.png',priceEther:1}
+    ],
+    market:[
+        {
+            "cardId": "",
+            "cardNo": 1103,
+            "cardName": "anti magic golem",
+            "cardNameJa": "アンチマジックゴーレム",
+            "attack": 2,
+            "hitPoint": 6,
+            "rarity": 3,
+            "cost": 3,
+            "attribute": 2,
+            "attackType": "",
+            "specials": [
+                2
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/anti-magic-golem.png",
+            "isMaster": "",
+            "price(ETH)": 0.05
+        },
+        {
+            "cardId": "",
+            "cardNo": 1104,
+            "cardName": "berserker",
+            "cardNameJa": "バーサーカー",
+            "attack": 3,
+            "hitPoint": 5,
+            "rarity": 3,
+            "cost": 3,
+            "attribute": 1,
+            "attackType": "",
+            "specials": [
+                5
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/berserker.png",
+            "isMaster": "",
+            "price(ETH)": 0.05
+        },
+        {
+            "cardId": "",
+            "cardNo": 1105,
+            "cardName": "gale swordsman（ninja）",
+            "cardNameJa": "疾風の剣士",
+            "attack": 3,
+            "hitPoint": 3,
+            "rarity": 2,
+            "cost": 3,
+            "attribute": 3,
+            "attackType": "",
+            "specials": [
+                4
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/gale-swordsman.png",
+            "isMaster": "",
+            "price(ETH)": 0.025
+        },
+        {
+            "cardId": "",
+            "cardNo": 1106,
+            "cardName": "flash swordman（holy knight）",
+            "cardNameJa": "閃光の剣士",
+            "attack": 3,
+            "hitPoint": 3,
+            "rarity": 2,
+            "cost": 2,
+            "attribute": 3,
+            "attackType": "",
+            "specials": [
+                3
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/flash-swordman.png",
+            "isMaster": "",
+            "price(ETH)": 0.025
+        },
+        {
+            "cardId": "",
+            "cardNo": 1107,
+            "cardName": "fairly ",
+            "cardNameJa": "フェアリー",
+            "attack": 1,
+            "hitPoint": 1,
+            "rarity": 2,
+            "cost": 1,
+            "attribute": 3,
+            "attackType": "",
+            "specials": [
+                7
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/fairly.png",
+            "isMaster": "",
+            "price(ETH)": 0.025
+        },
+        {
+            "cardId": "",
+            "cardNo": 1108,
+            "cardName": "curse warrier",
+            "cardNameJa": "カースウォーリア",
+            "attack": 2,
+            "hitPoint": 3,
+            "rarity": 2,
+            "cost": 2,
+            "attribute": 2,
+            "attackType": "",
+            "specials": [
+                8
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/curse-warrier.png",
+            "isMaster": "",
+            "price(ETH)": 0.025
+        },
+        {
+            "cardId": "",
+            "cardNo": 1109,
+            "cardName": "quick hit archer",
+            "cardNameJa": "早打ちのアーチャー",
+            "attack": 2,
+            "hitPoint": 2,
+            "rarity": 2,
+            "cost": 3,
+            "attribute": 1,
+            "attackType": 1,
+            "specials": [
+                1
+            ],
+            "imageURL": "https://d1wwbe44qvngmh.cloudfront.net/images/quick-hit-archer.png",
+            "isMaster": "",
+            "price(ETH)": 0.025
+        }
     ]
 }
 
--- src/frontend/market/page.js
+++ src/frontend/market/page.js
@@ -8,6 +8,7 @@ import Footer from "../component/footer";
 class Market extends Component {
     state = {
         preSaleCards: sellCards.preSale,
+        marketCards: sellCards.market,
         transactions: {},
     };
 
@@ -28,12 +29,24 @@ class Market extends Component {
                     tokens: tokens,
                     remains: remainingTokens
                 }
+            }),
+            marketCards: await asyncMap(sellCards.market, async card => {
+                const tokens = await BlockChainApi.listTokenIdsByCardNo(card.cardNo);
+                const remainingTokens = await asyncFilter(tokens, async token => {
+                    const price = Number.parseInt(await BlockChainApi.getPriceByTokenId(token));
+                    return price !== 0
+                });
+                return {
+                    ...card,
+                    tokens: tokens,
+                    remains: remainingTokens
+                }
             })
         })
     }
 
     render() {
-        const {preSaleCards, isBlockChainDataLoaded, transactions} = this.state;
+        const {preSaleCards, marketCards, isBlockChainDataLoaded, transactions} = this.state;
         return (
             <div className="pusher">
                 <Header activeIndex={1}/>
@@ -44,8 +57,9 @@ class Market extends Component {
                     <div className='ui grid'>
                         {preSaleCards.map(card => {
                             return <div className='ui eight wide column center aligned' key={card.cardNo}>
+                                <h3 className="ui white header">{card.cardNameJa}</h3>
                                 <img src={card.imageURL}
-                                     alt={card.cardName}
+                                     alt={card.cardNameJa}
                                      className='ui fluid center aligned image'/>
                                 {isBlockChainDataLoaded &&
                                 <div className='ui gray text'>
@@ -81,6 +95,46 @@ class Market extends Component {
                     </div>
                     <h3 className="ui white header"><FormattedMessage id='market.preSale2Header'/></h3>
                     <p className="gray"><FormattedMessage id='market.preSale2Text'/></p>
+                    <div
+                        className="ui grid">
+                        {marketCards.map(card => {
+                            return <div className='ui eight wide column center aligned' key={card.cardNo}>
+                                <h3 className="ui white header">{card.cardNameJa}</h3>
+                                <img src={card.imageURL}
+                                     alt={card.cardNameJa}
+                                     className='ui fluid center aligned image'/>
+                                {isBlockChainDataLoaded &&
+                                <div className='ui gray text'>
+                                    <FormattedMessage id='market.price'/>:{card.priceEther}ether
+                                    <br/>
+                                    <FormattedMessage
+                                        id='market.remains'/>:{card.remains.length}/<FormattedMessage
+                                    id='market.totalSupply'/>:{card.tokens.length}</div>}
+                                <button
+                                    className={"ui large primary center aligned button " + (isBlockChainDataLoaded && card.remains && card.remains.length === 0 && "disabled")}
+                                    onClick={async e => {
+                                        await this.onPurchase(card.cardNo)
+                                    }}>
+                                    <FormattedMessage id='market.purchase'/>
+                                </button>
+                                {transactions[card.cardNo] &&
+                                <div
+                                    className={"ui message " + (transactions[card.cardNo].status === 'processing' ? 'warning' :
+                                        transactions[card.cardNo].status === 'success' ? 'positive' : 'negative')}>
+                                    <i className="close icon" onClick={this.onCloseMessage}/>
+                                    <div className="header">
+                                        transaction {transactions[card.cardNo].status}
+                                    </div>
+                                    {transactions[card.cardNo].transactionHash && <p>confirm following transaction:
+                                        <a href={'https://etherscan.io/tx/' + transactions[card.cardNo].transactionHash}>see
+                                            in etherscan</a>
+                                    </p>}
+                                    {transactions[card.cardNo].error && <p>{transactions[card.cardNo].error}
+                                    </p>}
+                                </div>}
+                            </div>
+                        })}
+                    </div>
                     <h3 className="ui white header"><FormattedMessage id='market.preSale3Header'/></h3>
                     <p className="gray"><FormattedMessage id='market.preSale3Text'/></p>
                     <h3 className="ui white header"><FormattedMessage id='market.preSale4Header'/></h3>
