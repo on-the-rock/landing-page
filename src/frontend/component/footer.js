588c42f8d2a76cffc17d19b9ef6c5c0bc7c08c1d
seki <schokolade0317@gmail.com>
2019-09-27 20:29:20 +0900

    add contact

--- src/frontend/component/footer.js
+++ src/frontend/component/footer.js
@@ -8,9 +8,12 @@ class Footer extends Component {
                 <img src='https://d1wwbe44qvngmh.cloudfront.net/images/footer.png'
                      alt="footer"
                      className="ui image"/>
-                <p className="gray footer-right">
+                <p className="white footer-right">
+                    <div><i className="envelope icon"/> <a href='mailto:info@framecbattle.com'>info@framecbattle.com</a></div>
+                    <div><i className="twitter icon"/> <a href='https://twitter.com/frame_c_battle'>@frame_c_battle</a></div>
+                    <div><i className="medium icon"/> <a href='https://medium.com/frame-c-battle'>@framecbattle</a></div>
                     <FormattedMessage
-                    id="top.companyName"/></p>
+                        id="top.companyName"/></p>
                 <img src="https://d1wwbe44qvngmh.cloudfront.net/images/Frame+C+Battle.png"
                      alt="frame c battle" className="footer-left"/>
             </div>
